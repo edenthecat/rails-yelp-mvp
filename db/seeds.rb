67ff8b11072a8822ab9642192f8b4373b93d7403
Eden Rohatensky <edenrohatensky@gmail.com>
2017-11-01 17:16:08 -0400

    Completed controller and views for Restaurants.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+restaurant_attributes = [
+  {
+    name:   "Jesse's Diner",
+    address: "Jesse Lane",
+    phone_number: "6875309",
+    category: "japanese"
+  },
+  {
+    name:   "Alex's Diner",
+    address: "Alex Blvd",
+    phone_number: "6875309",
+    category: "chinese"
+  },
+    {
+    name:   "Hamza's Diner",
+    address: "Hamza's Avenue",
+    phone_number: "6875309",
+    category: "belgian"
+  },
+    {
+    name:   "Lucia's Diner",
+    address: "1 5th Ave",
+    phone_number: "6875309",
+    category: "french"
+  },
+    {
+    name:   "Robin's Diner",
+    address: "Right here",
+    phone_number: "6875309",
+    category: "italian"
+  },
+]
+
+restaurant_attributes.each { |params| Restaurant.create!(params)}
