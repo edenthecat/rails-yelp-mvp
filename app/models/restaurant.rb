133eae353496faaf8fe64bab8e3c68805f74b0e7
Eden Rohatensky <edenrohatensky@gmail.com>
2017-11-01 17:45:34 -0400

    Finishing touches. Very excellemt.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,15 @@ class Restaurant < ApplicationRecord
   validates :address, presence: true
   validates :category, inclusion: { in: CATEGORIES, allow_nil: false }
   has_many :reviews, dependent: :destroy
+
+  def average_rating
+    average_rating = 0
+    if self.reviews.length > 0
+      self.reviews.each do |review|
+        average_rating += review.rating
+      end
+      average_rating = average_rating / self.reviews.length
+    end
+    average_rating
+  end
 end
